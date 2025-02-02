28ddb8d20d9719456d31ad1a4fd9af9e579f6181
miloszekxxdr <106984000+miloszekxxdr@users.noreply.github.com>
2025-02-02 21:51:19 +0100

    Update InjectBored.js
    adadadadad

--- InjectBored.js
+++ InjectBored.js
@@ -1,14 +1,15 @@
-(function () {
-    let _0x4a92 = ['innerHTML', 'createElement', 'src', 'style', 'position', 'fixed', 'top', '0', 'left', 'width', '100vw', 'height', '100vh', 'border', 'none', 'allow', 'autoplay', 'appendChild'];
-    document.body[_0x4a92[0]] = ''; 
-    let iframe = document[_0x4a92[1]]('iframe');
-    iframe[_0x4a92[2]] = atob('aHR0cHM6Ly93d3cueW91dHViZS5jb20vZW1iZWQvZFEwdzR3OVdnWGM/QXV0b3BsYXk9MSZsb29wPTE='); // No need to check my code you'll know what it does when you execute it...
-    iframe[_0x4a92[3]][_0x4a92[4]] = _0x4a92[5];
-    iframe[_0x4a92[3]][_0x4a92[6]] = _0x4a92[7];
-    iframe[_0x4a92[3]][_0x4a92[8]] = _0x4a92[7];
-    iframe[_0x4a92[3]][_0x4a92[9]] = _0x4a92[10];
-    iframe[_0x4a92[3]][_0x4a92[11]] = _0x4a92[12];
-    iframe[_0x4a92[3]][_0x4a92[13]] = _0x4a92[14];
-    iframe[_0x4a92[15]] = _0x4a92[16];
-    document.body[_0x4a92[17]](iframe);
-})();
+// Deletes the whole webpage
+document.body.innerHTML = '';
+
+// Creates an iframe
+let iframe = document.createElement('iframe');
+iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1';
+iframe.style.position = 'fixed';
+iframe.style.top = '0';
+iframe.style.left = '0';
+iframe.style.width = '100vw';
+iframe.style.height = '100vh';
+iframe.style.border = 'none';
+iframe.allow = 'autoplay';
+
+document.body.appendChild(iframe);
